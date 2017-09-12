8d47a0fcad34b3c39fab8a71c3e8218a34078477
Sweet <vb520530@126.com>
2017-09-12 23:16:05 +0800

    update

--- src/main/java/com/njiu/cs/controller/UserController.java
+++ src/main/java/com/njiu/cs/controller/UserController.java
@@ -1,16 +1,26 @@
 package com.njiu.cs.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.njiu.cs.dao.UserDao;
 import com.njiu.cs.domain.User;
 
+/**
+ * demo
+ * @author sweet.hua
+ *
+ */
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
@@ -18,14 +28,50 @@ public class UserController {
 	@Resource
 	private UserDao userDao;
 
+	/**
+	 * 登录
+	 * @param model
+	 * @param session
+	 * @return
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public ModelAndView login(User model, HttpSession session) {
+	public ModelAndView login(User model, HttpSession session, HttpServletRequest request) {
 		User user = userDao.findByUsername(model.getLogin_name());
+		request.getSession(true).setAttribute("user", user); 
 		if (user !=null) {
 			return new ModelAndView("index");
 		} else {
 			return new ModelAndView("error");
 		}
 	}
+	
+	/**
+	 * 获取所有可用用户信息
+	 * @param model
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping(value = "/getUserList", method = RequestMethod.GET)
+	public ModelAndView getUserList(Model model, HttpSession session) {
+		List<User> userList = userDao.findAllUser();
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("userList", userList);
+		modelAndView.setViewName("user/user_list");
+		return modelAndView;
+	}
+	
+	/**
+	 * 删除用户
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/deleteUser", method = RequestMethod.GET)
+	public String deleteUser(@RequestParam("id") String id) {
+		userDao.deleteUser(id);
+		return "redirect:getUserList";
+	}
+	
+	
+	
 
 }
